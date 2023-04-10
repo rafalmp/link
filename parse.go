ada266c6c5d21df23647834d36902e0ea442b801
Rafał Peisert <rafalmp@users.noreply.github.com>
2023-04-10 14:53:53 +0200

    Depth-first search

--- parse.go
+++ parse.go
@@ -1,6 +1,11 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Link represents a HTML link tag (<a href="...">)
 type Link struct {
@@ -11,5 +16,22 @@ type Link struct {
 // Parse will accept a HTML document and will return a slice
 // of links extracted from it.
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	dfs(doc, "")
 	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(padding, msg)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"  ")
+	}
 }
